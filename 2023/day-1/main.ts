0b4f020a03452ef4aada4b51b5027f1e77d59833
Mike White <mike@mikehw.com>
2024-11-27 20:23:27 -0600

    fix up for publishing

--- 2023/day-1/main.ts
+++ 2023/day-1/main.ts
@@ -1,18 +1,15 @@
 import { Day, Solve } from "@mikehw/advent-of-code";
 import { assertEquals } from "@std/assert";
 
-const solution: Day = {
+const puzzle: Day = {
   year: 2023,
   day: 1,
   part1: (input: string) => {
     let sum = 0;
     for (const line of input.split("\n")) {
-      if (!line) {
-        continue;
-      }
       const digits = line.replaceAll(/[^0-9]/g, "");
-      const num =
-        parseInt(digits[0]) * 10 + parseInt(digits[digits.length - 1]);
+      const num = parseInt(digits[0]) * 10 +
+        parseInt(digits[digits.length - 1]);
       sum += num;
     }
     return sum.toString();
@@ -20,7 +17,7 @@ const solution: Day = {
 };
 
 if (import.meta.main) {
-  await Solve(solution);
+  await Solve(puzzle);
 }
 
 Deno.test("2023/day-1/part-1", async () => {
@@ -28,7 +25,5 @@ Deno.test("2023/day-1/part-1", async () => {
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
-  const expected = "142";
-  const actual = await solution.part1(input);
-  assertEquals(actual, expected);
+  assertEquals(await puzzle.part1(input), "142");
 });
--- advent-of-code/mod.ts
+++ advent-of-code/mod.ts
@@ -1,41 +1,54 @@
 import { join } from "@std/path";
+/**
+ * A module providing a function for advent of code puzzles
+ * @module
+ */
 
 /**
- * Advent of code two part challenge
+ * Advent of code two part puzzle
  */
 export interface Day {
-  // Year of the challenge
+  /** Year of the challenge */
   year: number;
-  // Day of the challenge
+  /**  Day of the challenge */
   day: number;
-  // Part 1 Solution
+  /** Part 1 Solution */
   part1: (input: string) => Promise<string> | string;
-  // Part 2 Solution
+  /** Part 2 Solution */
   part2?: (intput: string) => Promise<string> | string;
 }
 
-// Solve a day's solution
-// if input is not supplied, it will read from .inputs if available or fetch the input from the adventofcode.com,
-// fetching requires the ADVENT_OF_CODE_SESSION_COOKIE enviornment variable to be set,
+/**
+ * Solve a day's solution
+ * if input is not supplied, it will read from .inputs if available or fetch the input from the adventofcode.com,
+ * fetching requires the ADVENT_OF_CODE_SESSION_COOKIE enviornment variable to be set,
+ *
+ * @param day - Puzzle to Solve
+ * @param input - Optionally, the input for the puzzle
+ *
+ * ```ts
+ * if (import.meta.main) {
+ *   await Solve(puzzle);
+ * }
+ * ```
+ */
 export async function Solve(day: Day, input?: string) {
   if (!input) {
     input = await getInputForDay(day.year, day.day);
   }
-  console.log("Part 1:");
-  printPromise(day.part1(input));
+  console.log("Part 1:", day.part1(input));
   if (day.part2) {
-    console.log("Part 2:");
-    printPromise(day.part2(input));
+    console.log("Part 2:", day.part2(input));
   }
 }
 
 const CACHE_DIR = join(Deno.cwd(), ".inputs");
 
 async function getInputForDay(year: number, day: number): Promise<string> {
+  const cacheFile = join(CACHE_DIR, `${year}_${day}.txt`);
   try {
-    const file = join(CACHE_DIR, `${year}_${day}.txt`);
-    await Deno.lstat(file);
-    return await Deno.readTextFile(file);
+    await Deno.lstat(cacheFile);
+    return await Deno.readTextFile(cacheFile);
   } catch (err) {
     if (!(err instanceof Deno.errors.NotFound)) {
       throw err;
@@ -54,22 +67,16 @@ async function getInputForDay(year: number, day: number): Promise<string> {
     {
       headers,
       credentials: "include",
-    }
+    },
   ).catch((err) => {
     console.error(
       `Error fetching https://adventofcode.com/${year}/day/${day}/input: `,
-      err
+      err,
     );
     Deno.exit(1);
   });
-  const data = await resp.text();
+  const data = (await resp.text()).trim();
+  await Deno.mkdir(CACHE_DIR, { recursive: true });
+  await Deno.writeTextFile(cacheFile, data);
   return data;
-}
-
-async function printPromise(result: Promise<string> | string) {
-  if (result instanceof Promise) {
-    console.log(await result);
-  } else {
-    console.log(result);
-  }
 }
